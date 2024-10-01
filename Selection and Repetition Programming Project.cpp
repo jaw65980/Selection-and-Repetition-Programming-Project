99e7257c0e0adfcb2e18746a5131be83b7b579ef
Jaden White <jaw65980@email.vccs.edu>
2024-09-30 21:22:40 -0400

    Finished program

--- Selection and Repetition Programming Project.cpp
+++ Selection and Repetition Programming Project.cpp
@@ -5,6 +5,50 @@ using namespace std;
 
 int main()
 {
-    
+	float num1;
+	string oper;
+	int opereval;
+	float num2;
+
+	cout << "Enter a number." << endl;
+	cin >> num1;
+	cout << "Enter an operator. (+, -, /, or *)" << endl;
+	cin >> oper;
+
+	cout << "Enter a second number." << endl;
+	cin >> num2;
+
+	if (oper == "/" && num2 == 0)
+		opereval = 5;
+	else if (oper == "-")
+		opereval = 2;
+	else if (oper == "*")
+		opereval = 3;
+	else if (oper == "/")
+		opereval = 4;
+	else if (oper == "+")
+		opereval = 1;
+
+	switch (opereval)
+	{
+	case 1:
+		cout << "The result is: " << (num1 + num2);
+		break;
+	case 2:
+		cout << "The result is: " << (num1 - num2);
+		break;
+	case 3:
+		cout << "The result is: " << (num1 * num2);
+		break;
+	case 4:
+		cout << "The result is: " << (num1 / num2);
+		break;
+	case 5: 
+		cout << "Error: Divided by 0.";
+		break;
+	default:
+		cout << "Error: Invalid operation.";
+	}
+
 }
 
